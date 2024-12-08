ea1e04e2aed04d45b743dfef0a688a5c6f1826dc
Lmunif <kepsyelmunif.work@gmail.com>
2024-12-08 18:46:36 +0700

    test sintax git

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   // const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
