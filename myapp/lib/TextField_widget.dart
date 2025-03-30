b7c2aa40b0b8118b36fa0e62620be230175791a5
Lmunif <kepsyelmunif.work@gmail.com>
2025-03-30 17:06:43 +0700

    TextField (Decoration)

---
+++ myapp/lib/TextField_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool bolean   = true;
+  String tulisan   = 'Munculkan Pertama';
+
+
+  TextEditingController controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan Text Field"),),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextFormField(
+                  obscureText: bolean,
+                  onChanged: (value){
+                    setState(() {
+                      
+                    });
+                  },
+                  controller: controller,
+                ),
+                ElevatedButton(onPressed:(){
+                  // bolean = false;
+                  // tulisan = "sembunyikan";
+                  if (bolean == true ){
+                    tulisan = "sembunyikan";
+                    bolean = false;
+                  }
+                  else {
+                    tulisan = "munculkan";
+                    bolean = true;
+                  }
+                  setState(() {
+                      
+                    });
+                }, child: Text(tulisan)),
+                Text(controller.text)
+              ],
+          ),
+        ),
+      ),
+    );
+  }
+}
