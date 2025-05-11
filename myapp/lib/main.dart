f7503194729097d23f79713fd4f758726993c8e2
alwani achmad <alwaniachmad7031999aa@gmail.com>
2025-05-11 17:55:47 +0700

    Lanjutan Code kepsi

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
@@ -79,43 +80,124 @@ class MainPage extends StatelessWidget {
               child: ListView(
                 children: [
                   Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(5)),
-                      margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
-                      padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
-                      child: Column(
-                        children: [
-                          TextField(
-                            decoration: InputDecoration(
-                                icon: Icon(
-                                  Icons.email,
-                                  color: Color(0xFFFF4891),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(5)),
+                    margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.email,
+                                color: Color(0xFFFF4891),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Color(0xFFFF4891))),
+                              labelText: "Email: ",
+                              labelStyle: TextStyle(color: Color(0xFFFF4891))),
+                        ),
+                        TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.vpn_key,
+                                color: Color(0xFFFF4891),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Color(0xFFFF4891))),
+                              labelText: "Password: ",
+                              labelStyle: TextStyle(color: Color(0xFFFF4891))),
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
+                        child: Text(
+                          "Forgot Password?",
+                          style:
+                              TextStyle(color: Color(0xFFFF4891), fontSize: 11),
+                        ),
+                      )),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 0, 20, 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          height: 40,
+                          child: Container(
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.transparent,
+                              child: InkWell(
+                                borderRadius: BorderRadius.circular(20),
+                                splashColor: Colors.amber,
+                                onTap: () {},
+                                child: Center(
+                                  child: Text(
+                                    "SIGN IN",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700),
+                                  ),
                                 ),
-                                focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFFF4891))),
-                                labelText: "Email: ",
-                                labelStyle:
-                                    TextStyle(color: Color(0xFFFF4891))),
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color(0xFFB226B2),
+                                    Color(0xFFFF4891)
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                )),
                           ),
-                          TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                icon: Icon(
-                                  Icons.vpn_key,
-                                  color: Color(0xFFFF4891),
-                                ),
-                                focusedBorder: UnderlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Color(0xFFFF4891))),
-                                labelText: "Password: ",
-                                labelStyle:
-                                    TextStyle(color: Color(0xFFFF4891))),
-                          )
-                        ],
-                      ))
+                        ),
+                        FloatingActionButton(
+                            onPressed: () {},
+                            mini: true,
+                            elevation: 0,
+                            child: Image(
+                                image: AssetImage("assets/pictures/fb.png"))),
+                        FloatingActionButton(
+                            onPressed: () {},
+                            mini: true,
+                            elevation: 0,
+                            child: Image(
+                                image: AssetImage("assets/pictures/x.png")))
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't Have An Account? ",
+                        style: TextStyle(
+                            fontSize: 11,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w500),
+                      ),
+                        Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            fontSize: 11,
+                            color:Color(0xFFFF4891),
+                            fontWeight: FontWeight.w700),
+                      )
+                    ],
+                  )
                 ],
               ))
         ],
