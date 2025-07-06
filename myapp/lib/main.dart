96b17d305b6e853ffed8b46fba2bb0d53e6bb375
alwani achmad <alwaniachmad7031999aa@gmail.com>
2025-07-06 10:05:55 +0700

    get pull

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,108 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart'; 
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  AudioPlayer audioPlayer = AudioPlayer();
-  String durasi = "00:00:00"; 
-
-  @override
-  void initState() {
-    super.initState();
-
-    audioPlayer.onPositionChanged.listen((Duration p) {
-      setState(() {
-        durasi = p.toString().split('.').first; 
-      });
-    });
-  }
-
-  
-  Future<void> playSound(String url) async {
-
-    await audioPlayer.play(UrlSource(url));
-  }
-
-  // Fungsi untuk menjeda suara
-  Future<void> pauseSound() async {
-    await audioPlayer.pause();
-  }
-
-  // Fungsi untuk menghentikan suara
-  Future<void> stopSound() async {
-    await audioPlayer.stop();
-  }
-
-  // Fungsi untuk melanjutkan suara
-  Future<void> resumeSound() async {
-    await audioPlayer.resume();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Playing Music"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  playSound("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
-                },
-                child: Text("Play"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  pauseSound();
-                },
-                child: Text("Pause"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  stopSound();
-                },
-                child: Text("Stop"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  resumeSound();
-                },
-                child: Text("Resume"),
-              ),
-              Text(
-                "Durasi: $durasi",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              // Contoh untuk mencari waktu tertentu (misal 30 detik)
-              ElevatedButton(
-                onPressed: () {
-                  audioPlayer.seek(Duration(seconds: 30));
-                },
-                child: Text("Mulai dari 30 detik"),
-              ),
-            ],
-          ),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String text = "Hasil QR Scan";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Code Scanner"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: Text("Scan QR"),
+              onPressed: () {
+                // Aksi saat tombol ditekan
+              },
+            ),
+          ],
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    audioPlayer.release(); // Melepaskan sumber daya audio saat widget dibuang
-    super.dispose();
   }
 }
