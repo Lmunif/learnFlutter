c2fc328a5c8858defafde94a0b4a22930c16d8b5
Lmunif <kepsyelmunif.work@gmail.com>
2025-08-03 14:11:22 +0700

    error

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,46 +1,46 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:myapp/post_result_model.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PostResult postResult = null;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Custom Clipper"),
-        ),
+        appBar: AppBar(title: Text("API Demo"),),
         body: Center(
-          child: ClipPath(
-            clipper: MyClipper(),
-            child: Image(
-              width: 300,
-              image: NetworkImage(
-                  "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2020/11/29/caed1d5c-8017-43e0-a4cc-a66381c97cf9.jpg"),
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text((postResult != null ? postResult.id + "|" +
+              postResult.name + "|" +
+              postResult.job + "|" +
+              postResult.created : 'Tidak ada data ')),
+              ElevatedButton(onPressed: () {
+                PostResult.connectToAPI("Badu", "Dokter").then((value){
+                  postResult = value;
+                  setState(() {
+                    
+                  });
+                });
+              }, child: Text("POST"),
+              
+              ),
+            ],
           ),
         ),
       ),
     );
   }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-    path.lineTo(0, size.height);
-    path.quadraticBezierTo(size.width / 2, size.height * 0.75, size.width, size.height);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
