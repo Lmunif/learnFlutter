982180eac418a5461ebde4d0ce464522b3c49a56
alwani achmad <alwaniachmad7031999aa@gmail.com>
2025-08-09 10:17:49 +0700

    Tambah tombol input

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -15,31 +15,57 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   PostResult? postResult;
 
+  // Controller untuk inputan
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("API Demo")),
-        body: Center(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              TextField(
+                controller: _firstNameController,
+                decoration: const InputDecoration(labelText: "First Name"),
+              ),
+              TextField(
+                controller: _lastNameController,
+                decoration: const InputDecoration(labelText: "Last Name"),
+              ),
+              TextField(
+                controller: _ageController,
+                decoration: const InputDecoration(labelText: "Age"),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  String fname = _firstNameController.text;
+                  String lname = _lastNameController.text;
+                  String age = _ageController.text;
+
+                  if (fname.isNotEmpty && lname.isNotEmpty && age.isNotEmpty) {
+                    PostResult.connectToAPI(fname, lname, age).then((value) {
+                      setState(() {
+                        postResult = value;
+                      });
+                    });
+                  }
+                },
+                child: const Text("POST"),
+              ),
+              const SizedBox(height: 24),
               Text(
                 postResult != null
                     ? '${postResult!.id} | ${postResult!.firstName} | ${postResult!.lastName} | ${postResult!.age}'
-                    : 'Tidak ada datas',
+                    : 'Tidak ada data',
                 textAlign: TextAlign.center,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  PostResult.connectToAPI("alwani", "achmad", "25")
-                      .then((value) {
-                    setState(() {
-                      postResult = value;
-                    });
-                  });
-                },
-                child: const Text("POST"),
               ),
             ],
           ),
