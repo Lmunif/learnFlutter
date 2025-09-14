ae07696b7d62b00ac8ba366a7cad4c8013495e47
Lmunif <kepsyelmunif.work@gmail.com>
2025-09-14 21:00:02 +0700

    Provider State Management

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/cart_kepsy.dart';
+import 'package:myapp/money_kepsy.dart';
 import 'package:provider/provider.dart';
 import 'application_color.dart';
 
@@ -12,44 +14,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ChangeNotifierProvider<ApplicationColor>(
-        create: (context) => ApplicationColor(),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<MoneyKepsy>(
+            create: (context) => MoneyKepsy(),
+          ),
+          ChangeNotifierProvider<CartKepsy>(
+            create: (context) => CartKepsy(),
+          ),
+        ],
         child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Consumer<ApplicationColor>(
-              builder: (context, applicationColor, _) => Text(
-                "Provider State Management",
-                style: TextStyle(color: applicationColor.color),
+          floatingActionButton: Consumer<MoneyKepsy>(
+            builder: (context, money, _) => Consumer<CartKepsy>(
+              builder: (context, cart, _) => FloatingActionButton(
+                onPressed: () {            
+                  if (money.balance >= 500){
+                    cart.quantity += 1;
+                    money.balance -= 500;
+                  }
+                },
+                child: Icon(Icons.add_shopping_cart),
+                backgroundColor: Colors.pink,
               ),
             ),
+          ),
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+            title: Text("Multi Provider"),
           ),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Consumer<ApplicationColor>(
-                  builder: (context, applicationColor, _) => AnimatedContainer(
-                      margin: EdgeInsets.all(5),
-                      width: 100,
-                      height: 100,
-                      color: applicationColor.color,
-                      duration: Duration(milliseconds: 500)),
-                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(margin: EdgeInsets.all(5), child: Text("AB")),
-                    Consumer<ApplicationColor>(
-                      builder: (context, applicationColor, _) => Switch(
-                        value: applicationColor.isLightBlue,
-                        onChanged: (newValue) {
-                          applicationColor.isLightBlue = newValue;
-                        },
+                    Text("Balance"),
+                    Container(
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Consumer<MoneyKepsy>(
+                          builder: (context,MoneyKepsy, _) => Text(
+                            MoneyKepsy.balance.toString(),
+                            style: TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                      height: 30,
+                      width: 150,
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.purple[100],
+                          border: Border.all(color: Colors.purple, width: 2)),
+                    )
+                  ],
+                ),
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Consumer<CartKepsy>(builder: (context,CartKepsy, _) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Apple (500) X " + CartKepsy.quantity.toString(),
+                            style: TextStyle(
+                                color: Colors.black, fontWeight: FontWeight.w700),
+                          ),
+                          Text(
+                            (500 * CartKepsy.quantity).toString(),
+                            style: TextStyle(
+                                color: Colors.black, fontWeight: FontWeight.w700),
+                          )
+                        ],
                       ),
                     ),
-                    Container(margin: EdgeInsets.all(5), child: Text("LB"))
-                  ],
+                  ),
+                  height: 30,
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(color: Colors.black, width: 2)),
                 )
               ],
             ),
