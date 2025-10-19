ac7956b2709a87347de8c206e41ddd58a2f2f596
Lmunif <kepsyelmunif.work@gmail.com>
2025-10-19 09:54:38 +0700

    error

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -4,103 +4,68 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isStop = true;
-  bool isBlack = true;
-  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Timer Demo"),
+          title: Text("Custoom Progres bar"),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                counter.toString(),
-                style: TextStyle(
-                    color: isBlack == true && isStop == true ? Colors.black : (counter % 2 == 0 && isStop == false)
-                            ? Colors.blue
-                            : Colors.red,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    Timer(Duration(seconds: 5), () {
-                      setState(() {
-                        isBlack = !isBlack;
-                      });
-                    });
-                  },
-                  child: Text("Ubah Warna 5 Detik Kemudian")),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    Timer.run(() {
-                      setState(() {
-                        isBlack = !isBlack;
-                      });
-                    });
-                  },
-                  child: Text("Ubah Warna Secara Langsung")),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    counter = 0;
-                    isStop = false;
-                    Timer.periodic(Duration(seconds: 1), (Timer) {
-                      if (isStop == true) Timer.cancel();
-
-                      setState(() {
-                        counter++;
-                      });
-                    });
-                  },
-                  child: Text("Start Timer")),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      isStop = true;
-                    });
-                  },
-                  child: Text("Stop Timer")),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (isStop == true) {
-                        counter = 0;
-                      }
-                    });
-                  },
-                  child: Text("Reset")),
-            ],
-          ),
+          child: CustomProgressBar(width: 200, value: 10, totalValue: 100),
         ),
       ),
+    );
+  }
+}
+
+class CustomProgressBar extends StatelessWidget {
+  final double width;
+  final double value;
+  final double totalValue;
+
+  CustomProgressBar({this.width, this.value, this.totalValue});
+
+  @override
+  Widget build(BuildContext context) {
+    double ratio = value / totalValue;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.timer,
+          color: Colors.grey[700],
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Stack(
+          children: [
+            Container(
+              width: width,
+              height: 10,
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(5)),
+            ),
+            Material(
+              borderRadius: BorderRadius.circular(5),
+              elevation: 3,
+              child: AnimatedContainer(
+                height: 10,
+                width: width * ratio,
+                duration: Duration(milliseconds: 500),
+                decoration: BoxDecoration(
+                    color: Colors.lightGreen,
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
